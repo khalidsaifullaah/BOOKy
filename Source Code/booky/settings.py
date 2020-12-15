137b73ab0b1533a376523a144cab8af885ef464f
Khalid Saifullah <khalid277144@gmail.com>
2020-12-15 21:01:24 +0600

    added user Auth

--- Source Code/booky/settings.py
+++ Source Code/booky/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'users',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -118,4 +121,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/SAD Project/Source Code/users/static/users/',
+]
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGIN_URL = 'login'
---
+++ Source Code/users/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image, ImageOps
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import sys
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.TextField(default="", max_length=80)
+    contact_no  = models.CharField(max_length=11, null=True, blank=True)
+    buyer_rating = models.IntegerField(default=0, null=True, blank=True)
+    seller_rating = models.IntegerField(default=0, null=True, blank=True)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        image = Image.open(self.image.path)
+
+        # ImageOps compatible mode
+        if image.mode not in ("L", "RGB"):
+            image = image.convert("RGB")
+
+        imagefit = ImageOps.fit(image, (200, 200), Image.ANTIALIAS)
+        imagefit.save(self.image.path)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
