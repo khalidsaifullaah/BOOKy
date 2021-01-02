f46f499f2d3e1f5d5295deb0f6b688e798470b42
Khalid Saifullah <khalid277144@gmail.com>
2021-01-02 23:07:36 +0600

    front-end tweaks

--- Source Code/main/filters.py
+++ Source Code/main/filters.py
@@ -1,10 +1,11 @@
 import django_filters
 from .views import Book
+from django.forms.widgets import TextInput
 
 class BookFilter(django_filters.FilterSet):
 
-    title = django_filters.CharFilter(field_name='title', lookup_expr='icontains')
-    authors = django_filters.CharFilter(field_name='authors', lookup_expr='icontains')
+    title = django_filters.CharFilter(field_name='title', label='', lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'Title contains'}))
+    authors = django_filters.CharFilter(field_name='authors', label='', lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'Authors contain'}))
     #price_lower_than = django_filters.NumberFilter(field_name='price', lookup_expr='lt')
     #price_greater_than = django_filters.NumberFilter(field_name='price', lookup_expr='gt')
 
--- Source Code/main/views.py
+++ Source Code/main/views.py
@@ -20,24 +20,6 @@ class HomePage(FilterView):
     paginate_by = 6
 
 
-# @login_required
-# def uploadBook(request):
-
-#     if request.method == 'POST':
-#         #print('aaa', request.POST)
-#         form = BookUploadForm(request.POST)
-#         if form.is_valid():
-#             book = form.save(commit=False)
-#             book.uploader = request.user
-#             book.save()
-#             messages.success(request, 'Book uploaded successfully!')
-#             return redirect('home')
-#         else:
-#             print('vugi', form)
-
-#     form = BookUploadForm()
-#     return render(request, 'main/upload_book.html', {'form': form})
-
 class BookUploadView(LoginRequiredMixin, CreateView):
     model = Book
     template_name = 'main/upload_book.html'
