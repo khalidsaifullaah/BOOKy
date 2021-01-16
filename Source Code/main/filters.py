3c67363cba56eed2b621a1726eef514763f7b229
Khalid Saifullah <khalid277144@gmail.com>
2021-01-16 23:53:11 +0600

    code cleanup a bit

--- Source Code/main/filters.py
+++ Source Code/main/filters.py
@@ -6,8 +6,6 @@ class BookFilter(django_filters.FilterSet):
 
     title = django_filters.CharFilter(field_name='title', label='', lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'Title contains'}))
     authors = django_filters.CharFilter(field_name='authors', label='', lookup_expr='icontains', widget=TextInput(attrs={'placeholder': 'Authors contain'}))
-    #price_lower_than = django_filters.NumberFilter(field_name='price', lookup_expr='lt')
-    #price_greater_than = django_filters.NumberFilter(field_name='price', lookup_expr='gt')
 
     class Meta:
         model = Book
